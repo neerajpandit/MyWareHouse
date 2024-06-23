e48d050d1100f65ddfe37ba169808b9ece4238cd
neerajpandit <pandeyneeraj1507@gmail.com>
2024-06-23 15:58:39 +0530

    add cht

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,47 +1,48 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Message } from "../models/Chat.model.js";
 
+// Function to handle socket connections
 const handleSocketConnection = (io) => {
-    io.on("connection", (socket) => {
-      console.log("a user connected");
-  
-      socket.on("sendMessage", async (msg) => {
-        const newMessage = new Message({
-          senderId: msg.senderId,
-          recipientId: msg.recipientId,
-          timestamp: new Date(),
-          content: msg.content,
-          status: "sent",
-          conversationId: msg.conversationId,
-          reactions: [],
-          threadId: msg.threadId,
-          forwardedFrom: msg.forwardedFrom,
-        });
-  
-        try {
-          const savedMessage = await newMessage.save();
-          io.emit("message", savedMessage);
-        } catch (error) {
-          console.error("Error saving message", error);
-        }
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("sendMessage", async (msg) => {
+      const newMessage = new Message({
+        senderId: msg.senderId,
+        recipientId: msg.recipientId,
+        timestamp: new Date(),
+        content: msg.content,
+        status: "sent",
+        conversationId: msg.conversationId,
+        reactions: [],
+        threadId: msg.threadId,
+        forwardedFrom: msg.forwardedFrom,
       });
-  
-      socket.on("disconnect", () => {
-        console.log("user disconnected");
-      });
+
+      try {
+        const savedMessage = await newMessage.save();
+        io.emit("message", savedMessage);
+      } catch (error) {
+        console.error("Error saving message", error);
+      }
     });
-  };
-  
-  // Chat controller to handle fetching messages
-  const chat = asyncHandler(async (req, res) => {
-    try {
-      const messages = await Message.find({
-        conversationId: req.params.conversationId,
-      });
-      res.json(messages);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
+
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
   });
-  
-  export { chat, handleSocketConnection };
+};
+
+// Chat controller to handle fetching messages
+const chat = asyncHandler(async (req, res) => {
+  try {
+    const messages = await Message.find({
+      conversationId: req.params.conversationId,
+    });
+    res.json(messages);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+export { chat, handleSocketConnection };
--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { chat } from "../controllers/chat.controller.js";
+import { chat} from "../controllers/chat.controller.js";
 
 const router = Router();
 
-router.route("/:conversationId").get(chat);
+router.route("/message/:conversationId").get(chat);
 
 
 
